17836cdd78278349b24b6a114a5ac1cf9d248d50
Dong Liang <dong.liang@everbridge.com>
2021-05-08 23:16:29 -0400

    Update publish script

--- publish_build.sh
+++ publish_build.sh
@@ -2,6 +2,8 @@
 
 mvn clean package
 cp target/generated/js/teavm/classes.js docs/teavm/classes.js
+cp target/sorting-animation-1.0-SNAPSHOT/index.html docs/index.html
 git add docs/teavm/classes.js
+git add docs/index.html
 git commit -m "update build"
 git push origin master
